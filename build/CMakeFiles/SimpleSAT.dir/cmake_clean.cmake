cbf810b9e45666c4ff42eecb10fcfe8906944164
Daniel <s6dafabe@uni-bonn.de>
2024-11-07 12:03:35 +0100

    init

---
+++ build/CMakeFiles/SimpleSAT.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleSAT.dir/main.cpp.o"
+  "CMakeFiles/SimpleSAT.dir/main.cpp.o.d"
+  "SimpleSAT"
+  "SimpleSAT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleSAT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/SimpleSAT.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SimpleSAT.
+# This may be replaced when dependencies are built.
